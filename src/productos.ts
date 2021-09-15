c8ea5de6ecb907752e201fbe3c59828ffe110436
edborges <edgard.borges@mercadolibre.com>
2021-09-14 23:10:32 -0300

    desafio20_v1

--- src/productos.ts
+++ src/productos.ts
@@ -1,5 +1,7 @@
 /*Creamos nuestra clase con los metodos a utilizar 
 en la carpeta index.js*/
+import { DataTypeNotSupportedError } from 'typeorm';
+import { Producto } from './entities/formatoknex';
 import * as model from './schema/productschema'
 
 
@@ -13,12 +15,10 @@ class ProductosPersistencia {
     const producto = await model.productsSchema.find();
     return producto;
   }
-  async leerPorId(id:number) {
-    const producto = await model.productsSchema
-    .where('id')
-    .limit(1)
-    .select();
-    
+  async leerPorId(id:any) {
+    const producto = await model.productsSchema.findById({})
+    // .where('id')
+    // .limit(1)
 
     if(producto.length > 0){
       return producto[0];
@@ -28,6 +28,7 @@ class ProductosPersistencia {
   async guardar(dato:any) {
     
     const producto = {
+      
       nombre: dato.nombre,
       precio: dato.precio,
       url: dato.url
@@ -39,13 +40,18 @@ class ProductosPersistencia {
   }  
   async actualizar(dato:any, id:any){
     
-    const existProducto = await model.productsSchema
-    .find()
-    .where(id)
+    const existProducto:Producto[] = await model.productsSchema
+    .find({})
+    .where('id')
     .limit(1)
+    .select();
+    // .findById(_id)
+    // .where('_id','=',_id)
+    // .limit(1)
     
-
-    if(existProducto.length === 0){
+    
+    console.log(existProducto.length);
+    if(existProducto.length == 0){
       return {"error": "Producto no ha sido encontrado"};
     }    
     const producto = {
@@ -56,29 +62,29 @@ class ProductosPersistencia {
 
     // await productoRepo.save(producto)
     await model.productsSchema
-    .find()
-    .where(id)
+    .findById(id)
+    .where('id','=',id)
     .update(producto);
 
     return producto
     
   }
 
-  async borrar(id:any) {
+  async borrar(_id:any) {
     const producto = await model.productsSchema
-    .find()
-    .where('id','=',id)
+    .findById(_id)
+    .where('_id','=',_id)
     .limit(1)
-    .select();
     
-    if(!producto){
+    
+    if(!_id){
       return {"error": "Producto no ha sido encontrado"};
     }    
     
     await model.productsSchema
-    .find()
-    .where('id','=',id)
-    .del();
+    .findById(_id)
+    .where('_id','=',_id)
+    .deleteOne(producto);
     return producto
   } 
     
